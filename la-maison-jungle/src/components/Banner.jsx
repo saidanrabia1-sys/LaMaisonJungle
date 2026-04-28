799a2cb57601dc5e942fd2dad7d09b320d50f60f
unknown <saidanrabia1@gmail.com>
2026-04-28 16:37:14 +0300

    création de banner

---
+++ la-maison-jungle/src/components/Banner.jsx
@@ -0,0 +1,12 @@
+/**
+ * Banner est le composant pour présenter la bannière (la barre de navigation)
+ * Le composant Banner retourne un message codé en HTML <h1>Bienvenue à la maison</h1>
+ */
+const Banner = () => {
+    return <h1>Bienvenue à la maison jungle</h1>
+}
+
+/**
+ * J'exporte le composant banner en utilisant les mots-clès export default suivi du nom du composant
+ */
+export default Banner
