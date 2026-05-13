fe97b2735f5a72b15ef96b30941707beba01bfa0
unknown <saidanrabia1@gmail.com>
2026-05-13 12:08:55 +0300

    ajout des evenement et photo . utilisation des propos

--- la-maison-jungle/src/components/Cart.jsx
+++ la-maison-jungle/src/components/Cart.jsx
@@ -4,20 +4,36 @@
  */
 import styles from '../styles/Cart.module.css'
 
+import { useState } from 'react'
+
 const Cart = () => {
-    const monstera = 8
+    const monsteraPrice = 8
     const lierre = 12
     const bouquet = 9
+    const [cart, updateCart] = useState(0)
+    const [open, setOpen] = useState(false)
 
-    return(
+
+    return open ? (
         <div className={styles.cart}>
+
+            <button className={styles.lmjCartToogleButton} onClick={() => setOpen(false)}> Fermer </button>
             <h2>Votre panier</h2>
+            
+            <div>
+                Monstrea: {monsteraPrice} €
+                <button className={styles.lmjCartAddButton} onClick={() => updateCart(cart + 1)}> Ajouter </button>
+            </div>
+
             <ul style={{ listStyle: 'none', padding: 0, margin: 0 }}>
-                <li>Monstera : {monstera} €</li>
-                <li>Lierre : {lierre} €</li>
-                <li>Bouquet de fleurs : {bouquet} €</li>
+               
             </ul>
-            <p>Total : {monstera + lierre + bouquet} €</p>
+            <p>Total : {monsteraPrice * cart} €</p>
+            <button onClick={() => updateCart(0)}> Valider </button>
+        </div>
+    ) : (
+        <div className={styles.lmjCartClosed}>
+        <button className={styles.lmjCartToogleButton} onClick={() => setOpen(true)}> Ouvrir le panier </button>
         </div>
     )
 }
